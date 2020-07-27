2ed52bcae306c26292fe6737e9344db3e64a16ae
chengjiaxiongkf <66402618+chengjiaxiongkf@users.noreply.github.com>
2020-07-27 15:18:34 +0800

    Contants.java submit

---
+++ springcloud-common/src/main/java/com/util/Contants.java
@@ -0,0 +1,8 @@
+package com.util;
+
+public class Contants {
+    //返回标识
+    public static final String RESULT_CODE = "resultCode";
+    //返回信息
+    public static final String RESULT_MSG = "resultMsg";
+}
